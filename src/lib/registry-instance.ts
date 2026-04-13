07eb63aa5cffffbbdd1203d90ec4d6d835712418
jj <jj@jjs-MacBook-Pro.local>
2026-04-13 15:52:49 +0100

    feat: register all ops and add Vercel cron config

--- src/lib/registry-instance.ts
+++ src/lib/registry-instance.ts
@@ -1,6 +1,9 @@
 import { createRegistry } from "./registry";
 import type { Op } from "./types";
+import { syncSignups } from "./ops/sync-signups";
+import { donutTracker } from "./ops/donut-tracker";
+import { memberExport } from "./ops/member-export";
 
-const ops: Op[] = [];
+const ops: Op[] = [syncSignups, donutTracker, memberExport];
 
 export const registry = createRegistry(ops);
